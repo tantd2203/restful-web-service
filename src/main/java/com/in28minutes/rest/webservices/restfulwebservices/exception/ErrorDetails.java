110907392638a31263b1e11d8ad1b37f0a9b55e2
tantd9 <tranduytan.220300@gmail.com>
2023-01-29 14:21:08 +0700

    fianl restfull web service

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/ErrorDetails.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/ErrorDetails.java
@@ -1,20 +1,29 @@
 package com.in28minutes.rest.webservices.restfulwebservices.exception;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
-import java.util.List;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ErrorDetails {
-    private LocalDateTime timestamp;
-    private String message;
-    private String details;
 
+	private LocalDateTime timestamp;
+	private String message;
+	private String details;
 
+	public ErrorDetails(LocalDateTime timestamp, String message, String details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
 
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public String getDetails() {
+		return details;
+	}
 }
